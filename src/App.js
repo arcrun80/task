e2eb648a42691898ebb01dc9a4494c56b41af209
Arcrun <arcrunnou80@gmail.com>
2023-05-16 19:36:35 +0400

    Create new component, and modified app.js

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import Users from './Components/Users/users';
 import Detalis from './Components/Users/detalis';
+import Todo from './Components/Todo/todo';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <Route  path = {"users"} element ={<Users/>}/>
          <Route  path = {"users/:id"} element ={<Detalis/>}/>  
       </Routes>    
+      <Todo/>
     </div>
   );
 }
